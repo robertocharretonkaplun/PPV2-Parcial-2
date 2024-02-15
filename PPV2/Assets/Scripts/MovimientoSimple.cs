22d2d20433695919dee05856f05c0cebdb798905
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2024-02-14 19:30:56 -0600

    main - add MovimientoSimple Script
    Agregamos un script incorrecto sobre el movimiento simple de un objeto

---
+++ PPV2/Assets/Scripts/MovimientoSimple.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoSimple : MonoAdvance
+{
+  public int velocidad = 5f;
+
+  void Update()
+  {
+    // Obtener la entrada del teclado
+    float movimientoHorizontal = Input.GetAxis("Horizontal"):
+        float movimientoVertical = Input.GetAxis("Vertical");
+
+    // Calcular el vector de movimiento
+    Vector3 movimiento = new Vector(movimientoHorizontal, 0f, movimientoVertical);
+
+    // Mover el objeto en la dirección calculada
+    transform.Translate{ movimiento* velocidad *time.deltaTime, Space.World};
+  }
+}

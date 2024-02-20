62e8b8a6e6389a1694fcb1201fa44b05444a8b68
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2024-02-19 19:29:47 -0600

    main - First steps for project

--- PPV2/Assets/Scripts/MovimientoSimple.cs
+++ PPV2/Assets/Scripts/MovimientoSimple.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovimientoSimple : MonoAdvance
+public class MovimientoSimple : MonoBehaviour
 {
-  public int velocidad = 5f;
+  //public int velocidad = 5f;
 
-  void Update()
-  {
-    // Obtener la entrada del teclado
-    float movimientoHorizontal = Input.GetAxis("Horizontal"):
-        float movimientoVertical = Input.GetAxis("Vertical");
+  //void Update()
+  //{
+  //  // Obtener la entrada del teclado
+  //  float movimientoHorizontal = Input.GetAxis("Horizontal"):
+  //      float movimientoVertical = Input.GetAxis("Vertical");
 
-    // Calcular el vector de movimiento
-    Vector3 movimiento = new Vector(movimientoHorizontal, 0f, movimientoVertical);
+  //  // Calcular el vector de movimiento
+  //  Vector3 movimiento = new Vector(movimientoHorizontal, 0f, movimientoVertical);
 
-    // Mover el objeto en la dirección calculada
-    transform.Translate{ movimiento* velocidad *time.deltaTime, Space.World};
-  }
+  //  // Mover el objeto en la dirección calculada
+  //  transform.Translate{ movimiento* velocidad *time.deltaTime, Space.World};
+  //}
 }
